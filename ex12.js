76faaa098081a52dcec2123762c15647aac34538
mcostarodrigo <m.costa.rodrigo.rc@gmail.com>
2022-12-19 22:25:23 -0300

    Novos exercicios

---
+++ ex12.js
@@ -0,0 +1,27 @@
+/*Faça um programa onde colocamos dois numeros ímpares e o programa imprima o resto da divisão. 
+Ex: 7/3 deve imprimir: 1, 25/5 deve imprimir: 0.Faça um programa onde entramos com dois números, 
+e ele imprime se o primeiro número é maior que o segundo. Ex: 2 e 5. Imprime: false.
+Faça um programa onde entramos com dois números, e ele imprime se o primeiro número é menor que o segundo.
+ Ex: 2 e 5. Imprime: true.*/
+
+ let num1 = 25
+ let num2 = 5
+
+//Programa1
+
+ /*let result = num1 % num2
+
+ console.log(result)*/
+
+ //Programa2
+
+ /*let comparison = num1 > num2
+
+ console.log(comparison)*/
+
+  //Programa3
+
+  let comparison2 = num1 < num2
+
+  console.log(comparison2)
+
---
+++ ex13.js
@@ -0,0 +1,8 @@
+/*[ ] Crie um programa que mostra o tamanho do Array. 
+Ex: const array = [0, 1] imprime: 2. const array = ["banana", "Maça", "laranja"] imprime: 3*/
+
+const array = ["banana", "Maça", "laranja", "limão"]
+
+result = array.length;
+
+console.log(result)

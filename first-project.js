fd1252aa66deb04626208003dd33cfcad3de2e54
mcostarodrigo <m.costa.rodrigo.rc@gmail.com>
2022-12-27 12:29:03 -0300

    Adicionando exemplo

--- first-project.js
+++ first-project.js
@@ -1,1 +1,18 @@
-console.log("Olá MUndo")
+/*EXERCÍCIO APRESENTADO NA AULA DE FOREACH
+
+const estud = [
+    {nome: "João", age: 30},
+    {nome: "Jonas", age: 31},
+    {nome: "Josue", age: 34},
+    {nome: "Pedro", age: 35},
+];
+
+let todasAsIdades = 0
+
+estud.forEach( (qualquePalavra, index) => { //Feito por arrow function
+    todasAsIdades += qualquePalavra.age
+} )
+
+const mediaDeIdade = todasAsIdades / estud.length
+
+console.log(`A média de idade das pessoas é de ${mediaDeIdade.toFixed(0)}`)*/

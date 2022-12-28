23f9a73a573a8674427a25068f8771952e57c08e
mcostarodrigo <m.costa.rodrigo.rc@gmail.com>
2022-12-28 09:21:33 -0300

    Correção exercício

--- ex27.js
+++ ex27.js
@@ -5,54 +5,56 @@
 
 let people = [{
     numberofChildrens: 0,
-    salary: 20.000
+    salary: 20000
 },
 {
     numberofChildrens: 2,
-    salary: 10.000
+    salary: 10000
 },
 {
     numberofChildrens: 2,
-    salary: 5.000
+    salary: 5000
 },
-{ 
-     numberofChildrens: 1,
-    salary: 6.000
-},
-{ 
+{
     numberofChildrens: 1,
-   salary: 2.000
+    salary: 6000
 },
-{ 
+{
+    numberofChildrens: 1,
+    salary: 2000
+},
+{
     numberofChildrens: 3,
-   salary: -2.000
+    salary: -2.000
 }
 ]
 
 function findAverageAndHighestSalary(peopleInformation) {
 
-let averageSalary = 0
-let averageChildren = 0 
-let highestSalary = 0
+    let averageSalary = 0
+    let averageChildren = 0
+    let highestSalary = 0
 
-for(let i = 0; i <= peopleInformation.length; i++ ){
+    for (let i = 0; i <= peopleInformation.length; i++) {
 
-    const salary = peopleInformation [i].salary
-    const children = peopleInformation [i].numberofChildrens
+        const salary = peopleInformation[i].salary
+        const children = peopleInformation[i].numberofChildrens
 
-    if (salary > highestSalary) highestSalary = salary
+        if (salary > highestSalary) highestSalary = salary
 
-    if (salary < 0) {
-        console.log(`Média de salário R$${(averageSalary / i).toFixed(0)}`) /*toFixed(0) não mostra casa decimal*/
-        console.log(`Média de filhos R$${(averageChildren / i).toFixed(0)}`)
-        console.log(`Maior salário R$${highestSalary}`)
-        break
+        if (salary < 0) {
+            console.log(`Média de salário R$${(averageSalary / i).toFixed(0)}`) /*toFixed(0) não mostra casa decimal*/
+            console.log(`Média de filhos R$${(averageChildren / i).toFixed(0)}`)
+            console.log(`Maior salário R$${highestSalary}`)
+            break
 
-    } else {
-        averageSalary = averageSalary + salary;
-        averageChildren += children
+        } else {
+            averageSalary = averageSalary + salary;
+            averageChildren += children
+        }
+
     }
 
 }
 
-}
+findAverageAndHighestSalary(people)
